a5102619e21e0e893132d02f53d63904d6f764a0
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-09-03 09:54:56 -0300

    build

---
+++ dist/connector.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const informix = require("informixdb");
+let informixConnect;
+class Connector {
+    constructor(config) {
+        this.dsn = `SERVER=${config.server};
+    DATABASE=${config.collation};
+    HOST=${config.host};
+    SERVICE=${config.port};
+    UID=${config.username};
+    PWD=${config.password};`;
+    }
+    connect() {
+        if (!informixConnect) {
+            try {
+                informixConnect = informix.openSync(this.dsn);
+            }
+            catch (e) {
+                console.log(e);
+            }
+        }
+    }
+    execute(type) {
+        let resultSet;
+        if (!informixConnect) {
+            this.connect();
+        }
+        switch (type) {
+            case "invoices":
+                resultSet = informixConnect.querySync("select * from locales_ext");
+                break;
+            case "clients":
+                resultSet = informixConnect.querySync("select * from locales_ext");
+                break;
+        }
+        informixConnect.closeSync();
+        return resultSet;
+    }
+}
+exports.default = Connector;
